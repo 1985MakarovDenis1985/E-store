a134b6d7b18a3e04f5fcaa4a7171aaad846ef17b
Denis, Makarov <makarovdenis07081985@gmail.com>
2018-11-24 19:34:35 +0200

    with pou pup

--- app.js
+++ app.js
@@ -2,104 +2,182 @@ const products = [
     {
         "name": "Snikers #1",
         "price": 103.50,
+        "data":1,
+        "sex":"women",
+        "data-check":1,
         "image": "bg-01.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
     },
     {
         "name": "Snikers #2",
         "price": 152.14,
+        "data":2,
+        "sex":"men",
+        "data-check":2,
         "image": "bg-02.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
     },
     {
         "name": "Snikers #3",
         "price": 202.22,
+        "data":3,
+        "sex":"women",
+        "data-check":3,
         "image": "bg-03.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
     },
     {
         "name": "Snikers #4",
         "price": 240.00,
+        "data":4,
+        "sex":"women",
+        "data-check":4,
         "image": "bg-04.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
     },
     {
         "name": "Snikers #5",
         "price": 180.30,
+        "data":5,
+        "sex":"women",
+        "data-check":5,
         "image": "bg-05.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
     {
         "name": "Snikers #6",
         "price": 186.50,
+        "data":6,
+        "sex":"children",
+        "data-check":6,
         "image": "bg-06.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
 
     {
         "name": "Snikers #7",
         "price": 111.60,
+        "data":7,
+        "sex":"men",
+        "data-check":7,
         "image": "bg-07.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
     {
         "name": "Snikers #8",
         "price": 169.64,
+        "data":8,
+        "sex":"women",
+        "data-check":8,
         "image": "bg-08.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
     {
         "name": "LeBron 16 #9",
         "price": 215.20,
+        "data":9,
+        "sex":"women",
+        "data-check":9,
         "image": "bg-09.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
     {
         "name": "Nike Zoom KD11 #10",
         "price": 245.00,
+        "data":10,
+        "sex":"men",
+        "data-check":10,
         "image": "bg-10.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
     {
         "name": "Snikers #11",
         "price": 109.30,
+        "data":11,
+        "sex":"children",
+        "data-check":11,
         "image": "bg-05.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
     {
         "name": "Snikers #12",
         "price": 198.50,
+        "data":12,
+        "sex":"women",
+        "data-check":12,
         "image": "bg-06.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
 
     {
         "name": "Snikers #13",
         "price": 128.69,
+        "data":13,
+        "sex":"women",
+        "data-check":13,
         "image": "bg-01.jpg",
-        "description": "These are very cool snikers. Nice colorfull and quality"
+        "description": "These are very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
     {
         "name": "Snikers #14",
         "price": 179.00,
+        "data":14,
+        "sex":"men",
+        "data-check":14,
         "image": "bg-02.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
     {
         "name": "Snikers #15",
         "price": 260.20,
+        "data":15,
+        "sex":"children",
+        "data-check":15,
         "image": "bg-03.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice beautiful and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
 
     {
         "name": "Snikers #16",
         "price": 301.20,
+        "data":16,
+        "sex":"men",
+        "data-check":16,
         "image": "bg-03.jpg",
-        "description": "This is very cool snikers. Nice colorfull and quality"
+        "description": "This is very cool snikers. Nice colorfull and quality",
+        "more_description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor eaque magnam ut! Nam, temporibus vitae! Aspernatur dolores excepturi maiores neque numquam quasi voluptatibus."
+
     },
 ];
 
+
+//PAGINATION & SHOW ONLY 6 ITEMS
 
 let per_page = 6;
 let current = 0;
@@ -107,8 +185,9 @@ let current = 0;
 let pag_buttons = document.getElementsByClassName("page-link");
 let pagination = document.getElementById("pagination");
 let s = document.getElementById("s");
+let countText = document.getElementById("count");
 
-
+countText.innerHTML = "quantity in this store: " + products.length;
 
 
 // func - createPag
@@ -123,12 +202,10 @@ function createPag(array_products) {
     for (let i = 0; i < Math.ceil(array_products.length / per_page); i++) {
         link = createNewElement("a", i + 1, "page-link", [{"name": "data-link", "value": i}]);
 
-        activeEl =  (i == current) ? "page-item active" : "page-item";
+        activeEl = (i == current) ? "page-item active" : "page-item";
         li = attachChilderToParent(createNewElement("li", null, activeEl), [link]);
-
         ul.appendChild(li);
-    }
-    ;
+    };
 
     // pagination.innerHTML = "";
     pagination.appendChild(ul);
@@ -137,7 +214,7 @@ function createPag(array_products) {
         el.addEventListener("click", (e) => {
             console.log(e.target.dataset.link);
             current = e.target.dataset.link;
-            renderProducts(array_products);
+            renderProducts(array_products); // сдесь была ошибка - генереровались кнопки только от главного массива
         });
     });
 };
@@ -145,20 +222,111 @@ function createPag(array_products) {
 
 
 
+// CREATE POU PAP
+
+function CreatePouPap() {
+    let buttonClass = document.getElementsByClassName("add_card");
+
+
+    let button;
+    let coastP;
+    let boxDesc;
+    let rowSecond;
+    let buyBox;
+    let imgP;
+    let name;
+    let buttonExit;
+    let buttonExitIcon;
+    let frontPage;
+    let colLeft, colRight;
+    let firstRow;
+    let blockDescription;
+    let mainBlockDescription = document.getElementById("main_box_ppu_pap");
+    let buttonExitIconGet
+
+    Array.from(buttonClass).map((el)=>{
+        el.addEventListener("click", (e)=>{
+            for (let i = 0;i<products.length;i++){
+                if(el.name == products[i].data){
+                    console.log("kjhg");
+
+                    name = createNewElement("h5", products[i].name, "nameSnickers");
+                    button = createNewElement("button", "Add to cart", "buy btn btn-primary");
+                    coastP = createNewElement("h5", "Coast: "+products[i].price+" $", "coast", [{"name": "href", "value": "#"}]);
+                    buyBox = attachChilderToParent(createNewElement("div", null, "buyBox col-xs-4 col-md-12 col-sm-12"), [button, coastP]);
+
+                    boxDesc = createNewElement("div", products[i].more_description, "description col-xs-8 col-md-12 col-sm-12");
+
+                    rowSecond = attachChilderToParent(createNewElement("div", null, "row"), [boxDesc, buyBox]);
+
+                    imgP = createNewElement("img", "", "img_snickers",[{"name": "src", "value": "images/"+products[i].image}]);
+
+                    buttonExitIcon = createNewElement("i", "", "fas fa-plus");
+                    buttonExit = attachChilderToParent(createNewElement("div", null, "exit",[{"name": "id", "value": "exitBox"}]), [buttonExitIcon]);
+
+                    colLeft = createNewElement("div", "", "col-xs-3 col-md-3 col-sm-3");
+
+                    colRight = createNewElement("div", "", "col-xs-3 col-md-3 col-sm-3");
+
+                    frontPage = attachChilderToParent(createNewElement("div", null, "front col-xs-6 col-md-6 col-sm-6"), [buttonExit, imgP, name, rowSecond]);
+
+                    firstRow = attachChilderToParent(createNewElement("div", null, "row"), [colLeft, frontPage, colRight]);
+
+
+                    blockDescription = attachChilderToParent(createNewElement("div", null, "block_description col-xs-12 col-md-12 col-sm-12"), [firstRow]);
+
+                    mainBlockDescription.appendChild(blockDescription)
+
+
+
+                    buttonExitIconGet = document.getElementById("exitBox");
+                    buttonExitIconGet.addEventListener("click", (e)=>{
+                        mainBlockDescription.innerHTML="";
+                        console.log("close");
+                    });
+                };
+            };
+        });
+    });
+}
+
+
+
+
+// SEARCH
 
 const search_button = document.getElementById("search");
 
     search_button.addEventListener("click",(e)=>{
+        // console.log("khjghj");
+        search_touch()
+    });
+
+    s.addEventListener("keypress", (e)=>{
+        if (e.keyCode == 13){
+            // console.log("jakjc");
+            search_touch();
+        };
+    });
+
+
+    function search_touch() {
         let products_filter;
 
         // console.log(s);
         const reg = new RegExp(s.value);
-        products_filter = products.filter((el) => reg.test(el.name));
+        products_filter = products.filter((el) => reg.test(el.name) || reg.test(el.description));
         current = 0;
         s.value = "";
         renderProducts(products_filter); // add
         createPag(products_filter);// add
-    });
+
+        countText.innerHTML = "your choose compose from:   " + products_filter.length + "  items";
+        // let y = document.getElementById("x");
+        // console.log(y);
+
+    };
+
 
 
 
@@ -172,6 +340,11 @@ function secondI(i,array_products) {
     return i < current * per_page + per_page && i < array_products.length;
 };
 
+
+
+
+//RENDER PRODUCTS
+
 var catalog = document.getElementById("catalog");
 
 function renderProducts(array_products) {
@@ -179,14 +352,58 @@ function renderProducts(array_products) {
     for (var i = firstI(); secondI(i,array_products); i++) {
         catalog.appendChild(createCard(array_products[i]));
     };
-    createPag(products);
+    createPag(array_products);
+
+    //POU-PAP
+    CreatePouPap()
+
 };
 
 
+//==================
+
+let z = document.getElementsByClassName("ttt");
+let products_fil;
+let men = "men";
+let women = "women";
+let child = "children"
+
+
+let y = Array.from(z).map((el)=>{
+  el.addEventListener("click", (e)=>{
+      if(el.id == 1){
+        console.log("click");
+        filter_sex(men);
+
+
+      }else if(el.id == 2){
+          filter_sex(women);
+          // createPag(products_fil);// add
+
+
+      }else if(el.id == 3){
+          // filter_women(child)
+          renderProducts(products);
+          countText.innerHTML = "quantity in this store: " + products.length;
+      }
+  })
+})
+
+
+function filter_sex(sex) {
+    products_fil = products.filter((el)=>el.sex == sex);
+    renderProducts(products_fil); // add
+    createPag(products_fil);// add
+    countText.innerHTML = "your choose compose from:   " + products_fil.length + "  items";
+}
 
 
 
+//==================
 
+
+
+// CREATE NEW ELEMENT
 
 function createNewElement(tag, innerContent = null, classStr = null, attr = null) {
     var el = document.createElement(tag);
@@ -200,12 +417,26 @@ function createNewElement(tag, innerContent = null, classStr = null, attr = null
     return el;
 }
 
+
+
+
+//CREATE CARD
+
 function createCard(product) {
-    var link = createNewElement("a", "Add to cart", "btn btn-primary", [{"name": "href", "value": "#"}]);
+
+    var link = createNewElement("a", "Add to cart", "btn btn-primary add_card", [{"name": "href", "value": "#"}]);
+    link.setAttribute("name",product.data);
+
+
     var p = createNewElement("p", product.description, "card-text");
     var title = createNewElement("h5", product.name, "card-title");
+    var coast = createNewElement("h3", "$ " + product.price, "");
 
-    var cardBody = attachChilderToParent(createNewElement("div", null, "card-body"), [title, p, link]);
+
+    var cardBody = attachChilderToParent(createNewElement("div", null, "card-body"), [title, p, coast]);
+
+    cardBody.appendChild(link)
+
 
     var image = createNewElement("img", null, "card-img-top", [{
         "name": "src",
@@ -217,17 +448,19 @@ function createCard(product) {
     var catalogItem = attachChilderToParent(createNewElement("div", null, "col-lg-3 col-md-6 mb-2 catalog-item"), [card]);
 
     return catalogItem;
-}
+};
+
+
+//CREATE PARRENTS
 
 function attachChilderToParent(html, array_el) {
     array_el.map((el) => html.appendChild(el));
     return html;
-}
-
-
+};
 
 
 renderProducts(products);
+
 
 
 
